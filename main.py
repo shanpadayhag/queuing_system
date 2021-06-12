df15d9d4075a5d2647d51b05e170fe2c9ff82cda
Shan Padayhag <shanpadayhag@gmail.com>
2021-06-12 19:21:48 +0800

    Version three initialization

--- main.py
+++ main.py
@@ -7,26 +7,15 @@ from PyQt5.QtQml import QQmlApplicationEngine
 # SELF CREATED MODULE IMPORTS
 from classes import splashScreen, login
 
-from PyQt5.QtCore import QObject,  pyqtSignal, pyqtSlot
-import classes.login as login
 
 # INSTANCE CLASS
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
 
-<<<<<<< HEAD
     SplashScreen = splashScreen.splashScreen()
 
     engine.rootContext().setContextProperty("SplashScreen", SplashScreen)
-=======
-    # Instance of a Python object
-    Login = login.LogIn()
-    # Expose the python object to QML
-    engine.rootContext().setContextProperty('login', Login)
-    
-    engine.load('qml/logIn.qml')
->>>>>>> master
 
     engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
     
